a24852b0f393c7f9d5dac40eaa15dff248ddc2ca
Den <33260586+DenCoder618@users.noreply.github.com>
2021-02-13 20:49:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,101 @@
-# -*- coding: utf-8 -*-
-import redis
 import os
 import telebot
-# import some_api_lib
-# import ...
+import requests
+import bs4 as bs
+import telebot
+import bs4
+from Task import Task
+from telebot import types
 
-# Example of your code beginning
-#           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+login = os.environ['WALKER_LOGIN']
+password = os.environ['WALKER_PASSWORD']
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+main_link = "https://w41k3r.com/"
+login_link = "https://w41k3r.com/wp-login.php"
+alan_link = "https://w41k3r.com/walkers/?w_id=0"
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+posts = []
+comments = []
+
+start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+start_markup_btn1 = types.KeyboardButton('w41k3r')
+#start_markup_btn2 = types.KeyboardButton('Comments')
+start_markup.add(start_markup_btn1)
+
+class Post:
+  def __init__(self, data):
+    #if data != '':
+    self.author = data.find('span').text
+    self.url = data.get('href')
+    self.comments = int(data.find('p').text.split(' – ')[0].split()[0])
+    self.date = data.find('p').text.split(' – ')[1][10:]
+    self.text = data.find('h2').text.split(' - ')[1]
+class Comment:
+  def __init__(self, data, author):
+    #if data != '':
+    self.author = author
+    self.url = data.get('href')
+    self.text = data.find('h2').text[1:-1]
+    self.responce = data.find_all('p')[0].text[16:-1]
+    self.date = data.find_all('p')[1].text[14:]
+
+def potato_parser:
+	c = requests.session()
+	initial = c.get(login_link)
+	login_data = {"log": login, "pwd": password,
+	              "rememberme": "forever",
+	              "redirect_to": main_link,
+	              "redirect_to_automatic": "1"}
+	
+	page_login = c.post(login_link, data=login_data)
+	print(page_login)
+	
+	page=c.get(alan_link)
+	soup = bs.BeautifulSoup(page.text)
+	main = soup.find('main', {'class': 'body-profile centered'})
+	post_data = main.find_all('div')[0].find_all('a')
+	comm_data = main.find_all('div')[1].find_all('a')
+	
+	posts = []
+	comments = []
+	for p in post_data:
+	  posts.append(Post(p))
+	for c in comm_data:
+	  comments.append(Comment(c, '#0'))
+
+def format_out:
+	output = "POSTS:\n"
+	for p in posts:
+		output += "[" + p.date + "]\n(" + p.author + ') - ' + p.text + "\n\n"
+	output += "COMMENTS\n"
+	for c in comments:
+		output += "[" + c.date + "]\n(" + p.author + ') - ' + p.text + "\n\n"
+	return output
+
+##### BOT #####
+
+bot = telebot.TeleBot(token)
+task = Task()
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+  #if not task.isRunning:
+  chat_id = message.chat.id
+  msg = bot.send_message(chat_id, 'Hello, potato!', reply_markup=start_markup)
+  bot.register_next_step_handler(msg, askSource)
+  #task.isRunning = True
+
+def askSource(message):
+  chat_id = message.chat.id
+  text = message.text()
+  if text == 'w41k3r' :
+  	potato_parser();
+  	msg = bot.send_message(chat_id, format_out(), reply_markup=start_markup)
+  	bot.register_next_step_handler(msg, askSource)
+  else:
+    msg = bot.send_message(chat_id, 'potato..?', reply_markup=start_markup)
+    bot.register_next_step_handler(msg, askSource)
+    return
+
+bot.polling(none_stop=True)
