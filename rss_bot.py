dc6d66b5a946dbcdb087fbeefaa6da6da193b7fc
Den <33260586+DenCoder618@users.noreply.github.com>
2021-02-25 17:28:06 +0300

    Update rss_bot.py

--- rss_bot.py
+++ rss_bot.py
@@ -28,14 +28,9 @@ def parse_rss(link):
         if not hash in old_hashes:
             p_link = entry['links'][0]['href']
             c = requests.session()
-            login_data = {"log": login,
-            "pwd": password,
-            "rememberme": "forever",
-            "redirect_to": main_link,
-            "redirect_to_automatic": "1"}
-            page_login = c.post(login_link, data=login_data)
+            login_data = {"log": login, "pwd": password, "rememberme": "forever", "redirect_to": main_link, "redirect_to_automatic": "1"} page_login = c.post(login_link, data=login_data)
             p = c.get(link)
-            s = bs.BeautifulSoup(p.text, features="html.parser").find("main")
+            s = bs.BeautifulSoup(p.content, features="html.parser").find("main")
             title = s.find("h1").text
             wid = s.find("span", {'class': 'wid'}).text
             t = s.find("article").text
